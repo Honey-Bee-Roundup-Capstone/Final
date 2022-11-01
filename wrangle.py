eda2cafde064c77eaa4297ff0f95d78792b66613
JenniferMLewis <lewis.jennifer.marie.15@gmail.com>
2022-11-01 16:48:21 -0500

    Added get_bee_data() function to pull the raw csv data we received from Bee Informed and turn it into the clean bee_colony_loss.csv

--- wrangle.py
+++ wrangle.py
@@ -4,6 +4,41 @@ import matplotlib.pyplot as plt
 import os
 import seaborn as sns
 
+def get_bee_data():
+    '''
+    Reads the Raw data of the Bee Informed State Bee Loss Table (2022) CSV,
+    Drops columns that contain data we don't plan to utilize.
+    Drops the first column since they're the 'Python Column Names',
+    Renames columns into a more Python friendly format,
+    Saves the new working CSV as bee_colony_loss.csv
+    '''
+    # Read Raw CSV:
+    df = pd.read_csv("BeeInformed_States_Loss_Table_by_Year_public_ready_2022.csv")
+    # Drops several columns with data from their statistical accounting, which is nice they included, but we're looking more for numbers to work with for our own stats.
+    df = df.drop(columns=["Column name as written in R Script", 'State abbreviation', "Method of tallying multi-states operations (included in all states, excluded from all states, exlusively multi-states)", 
+       'Bootstrap replication',
+       'Bootstrap method',
+       'Bootstrap estimate of the Total Loss (weigthed average)',
+       'Boostrap-based 95% confidence interval(low) of the weighted average loss',
+       'Boostrap-based 95% confidence interval(high) of the weighted average loss',
+       'Bootstrap estimate of the Average Loss (unweigthed average)',
+       'Boostrap-based 95% confidence interval(low) of the unweighted average loss',
+       'Boostrap-based 95% confidence interval(high) of the unweighted average loss',
+       'glm-based 95% confidence interval(low) of the weighted average loss',
+       'glm-based 95% confidence interval(high) of the weighted average loss',
+       'standard deviation of operational losses',
+       'standard error of the unweithed average estimate',
+       'glm-based 95% confidence interval(low) of the unweighted average loss',
+       'glm-based 95% confidence interval(high) of the unweighted average loss',
+       'Total number of colonies "at risk" (colonies at the start, new colonies added, without colonies sold or given away)'])
+    # Due to the format the CSV comes in, the first line is "Python" column names, so we'll drop the first row since we're stuck with the R column names for our columns.
+    df = df.iloc[1:, :]
+    # Renaming Columns to be more Python Friendly.
+    df.columns = ["state", "year", "season", "beekeepers", "total_loss", "average_loss", "starting_colonies", "colonies_lost", "ending_colonies", "beekeepers_exclusive_to_state", "colonies_exclusive_to_state"]
+    df.to_csv("bee_colony_loss.csv")
+    return df
+    
+
 def prep_bees():
     '''This function loads the bee_colony_loss.csv into a dataframe, cleans and sorts it, and returns a dataframe.'''
     # read the csv into a pandas dataframe
