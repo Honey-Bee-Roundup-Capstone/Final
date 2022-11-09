2b6b33e4da44b55005f5a5b9b62232b76c0421dd
JenniferMLewis <lewis.jennifer.marie.15@gmail.com>
2022-11-09 14:16:38 -0600

    updating Final notebook for QC

--- explore.py
+++ explore.py
@@ -22,6 +22,7 @@ from sklearn.metrics import mean_squared_error,explained_variance_score, r2_scor
 from sklearn.linear_model import LinearRegression,LassoLars,TweedieRegressor
 from sklearn.feature_selection import SelectKBest, RFE, f_regression
 from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -279,3 +280,62 @@ def select_rfe(X,y,  n_features_to_select = 4):
     features = X.columns[rfe.get_support()]
     
     return features
+
+#==================== Grid Search ===============================
+
+def lr_best_hyperparameter(X_train,y_train):
+    '''This function will input X,y and variation in hyperparameters and
+    outputs best hyperparameter'''
+    #define hyperparamters
+    params = {'normalize': [ True, False],
+          'fit_intercept': [True, False]}
+    #create grid object
+    grid = GridSearchCV(LinearRegression(), params, cv=5)
+    #fit grid object
+    grid.fit(X_train,y_train.colonies_lost)
+    #get best hyperparameters
+    best = grid.best_estimator_
+    
+    #return output
+    return best
+    
+
+def ll_best_hyperparameter(X_train,y_train):
+    '''This function will input X,y and variation in hyperparameters and
+    outputs best hyperparameter'''
+    #define hyperparamters
+    params = {
+          'normalize': [True, False],
+          'fit_intercept':[True, False],
+           'alpha': [1,2,3,4]
+         }
+    #create grid object
+    grid = GridSearchCV(LassoLars(), params, cv=5)
+    #fit grid object
+    grid.fit(X_train,y_train.colonies_lost)
+    #get best hyperparameters
+    best = grid.best_estimator_
+    
+    #return output
+    return best
+
+
+def tr_best_hyperparameter(X_train,y_train):
+    '''This function will input X,y and variation in hyperparameters and
+    outputs best hyperparameter'''
+    #define hyperparameters
+    params = {
+          'power': [0, 1,2,3],
+           'fit_intercept' : [True , False],
+          'warm_start': [True, False], 
+           'alpha': [1,2,3,4,5]
+}
+    #create grid object
+    grid = GridSearchCV(TweedieRegressor(), params, cv=5,scoring = 'neg_root_mean_squared_error')
+    #fit grid object
+    grid.fit(X_train,y_train.colonies_lost)
+    #get best parameters
+    best = grid.best_estimator_
+    
+    #return output
+    return best
